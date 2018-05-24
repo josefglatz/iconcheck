50444ed6ebd61704977f69c2b7e79731c41938d0
Josef Glatz <j.glatz@supseven.at>
2018-05-24 11:13:00 +0200

    [TASK] Update Ext

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,22 +1,29 @@
 <?php
-
-$EM_CONF[$_EXTKEY] = [
-    'title' => 'Icon Check: show registered icons',
-    'description' => 'Module for listing all registered icons',
-    'category' => 'be',
-    'author' => 'Josef Glatz',
-    'state' => 'stable',
-    'version' => '1.0.0',
-    'constraints' => [
-        'depends' => [
-            'typo3' => '8.7.0-8.7.99',
-        ],
-        'conflicts' => [],
-        'suggests' => [],
-    ],
-    'autoload' => [
-        'psr-4' => [
-            'JosefGlatz\\IconCheck\\' => 'Classes'
-        ]
-    ],
-];
+$EM_CONF[$_EXTKEY] = array (
+  'title' => 'Icon Check: show registered icons',
+  'description' => 'Module for listing all registered icons',
+  'category' => 'be',
+  'author' => 'Josef Glatz',
+  'state' => 'stable',
+  'version' => '1.0.1',
+  'constraints' => 
+  array (
+    'depends' => 
+    array (
+      'typo3' => '8.7.0-8.7.99',
+    ),
+    'conflicts' => 
+    array (
+    ),
+    'suggests' => 
+    array (
+    ),
+  ),
+  'autoload' => 
+  array (
+    'psr-4' => 
+    array (
+      'JosefGlatz\\IconCheck\\' => 'Classes',
+    ),
+  ),
+);
