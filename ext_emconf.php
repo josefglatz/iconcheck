fc4282fb1e1b7b69f63905805d0a46233b4fa94f
Josef Glatz <j.glatz@supseven.at>
2018-07-17 23:21:29 +0200

    [TASK] Raise TYPO3 core version constraint
    Support for TYPO3
    - 8.7 LTS
    - 9.1.0 - 9.3.99

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,29 +1,29 @@
 <?php
-$EM_CONF[$_EXTKEY] = array (
+$EM_CONF[$_EXTKEY] = [
   'title' => 'Icon Check: show registered icons',
-  'description' => 'Module for listing all registered icons',
+  'description' => 'Module for listing (all) registered icons',
   'category' => 'be',
   'author' => 'Josef Glatz',
   'state' => 'stable',
   'version' => '1.0.2',
-  'constraints' => 
-  array (
-    'depends' => 
-    array (
-      'typo3' => '8.7.0-8.7.99',
-    ),
-    'conflicts' => 
-    array (
-    ),
-    'suggests' => 
-    array (
-    ),
-  ),
-  'autoload' => 
-  array (
-    'psr-4' => 
-    array (
-      'JosefGlatz\\IconCheck\\' => 'Classes',
-    ),
-  ),
-);
+  'constraints' =>
+  [
+    'depends' =>
+    [
+      'typo3' => '8.7.0-9.3.99',
+    ],
+    'conflicts' =>
+    [
+    ],
+    'suggests' =>
+    [
+    ],
+  ],
+  'autoload' =>
+  [
+    'psr-4' =>
+    [
+      'JosefGlatz\\Iconcheck\\' => 'Classes',
+    ],
+  ],
+];
