b1a74a0517049f9c81487a8e041ed8f5e391f1a5
Josef Glatz <j.glatz@supseven.at>
2018-07-17 23:35:59 +0200

    [BUGFIX] Fix version in ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,29 +1,30 @@
 <?php
 $EM_CONF[$_EXTKEY] = [
-  'title' => 'Icon Check: show registered icons',
-  'description' => 'Module for listing (all) registered icons',
-  'category' => 'be',
-  'author' => 'Josef Glatz',
-  'state' => 'stable',
-  'version' => '1.0.2',
-  'constraints' =>
-  [
-    'depends' =>
-    [
-      'typo3' => '8.7.0-9.3.99',
-    ],
-    'conflicts' =>
-    [
-    ],
-    'suggests' =>
-    [
-    ],
-  ],
-  'autoload' =>
-  [
-    'psr-4' =>
-    [
-      'JosefGlatz\\Iconcheck\\' => 'Classes',
-    ],
-  ],
+    'title' => 'Icon Check: show registered icons',
+    'description' => 'Module for listing (all) registered icons',
+    'category' => 'be',
+    'author' => 'Josef Glatz',
+    'author_email' => 'jousch@gmail.com',
+    'state' => 'stable',
+    'version' => '2.0.1',
+    'constraints' =>
+        [
+            'depends' =>
+                [
+                    'typo3' => '8.7.0-9.3.99',
+                ],
+            'conflicts' =>
+                [
+                ],
+            'suggests' =>
+                [
+                ],
+        ],
+    'autoload' =>
+        [
+            'psr-4' =>
+                [
+                    'JosefGlatz\\Iconcheck\\' => 'Classes',
+                ],
+        ],
 ];
