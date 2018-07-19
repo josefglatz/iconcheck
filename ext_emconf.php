aebf14db9d631702aee9e8710ac3a05ca7e2236b
Josef Glatz <j.glatz@supseven.at>
2018-07-19 17:55:48 +0200

    [TASK] Update version
    .Build/bin/setversion 2.0.3

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,31 +1,31 @@
 <?php
-$EM_CONF[$_EXTKEY] = [
-    'title' => 'Icon Check: show registered icons',
-    'description' => 'Module for listing (all) registered icons',
-    'category' => 'be',
-    'author' => 'Josef Glatz',
-    'author_email' => 'jousch@gmail.com',
-    'author_company' => '',
-    'state' => 'stable',
-    'version' => '2.0.1',
-    'constraints' =>
-        [
-            'depends' =>
-                [
-                    'typo3' => '8.7.17-9.3.99',
-                ],
-            'conflicts' =>
-                [
-                ],
-            'suggests' =>
-                [
-                ],
-        ],
-    'autoload' =>
-        [
-            'psr-4' =>
-                [
-                    'JosefGlatz\\Iconcheck\\' => 'Classes',
-                ],
-        ],
-];
+$EM_CONF[$_EXTKEY] = array (
+  'title' => 'Icon Check: show registered icons',
+  'description' => 'Module for listing (all) registered icons',
+  'category' => 'be',
+  'author' => 'Josef Glatz',
+  'author_email' => 'jousch@gmail.com',
+  'author_company' => '',
+  'state' => 'stable',
+  'version' => '2.0.3',
+  'constraints' => 
+  array (
+    'depends' => 
+    array (
+      'typo3' => '8.7.17-9.3.99',
+    ),
+    'conflicts' => 
+    array (
+    ),
+    'suggests' => 
+    array (
+    ),
+  ),
+  'autoload' => 
+  array (
+    'psr-4' => 
+    array (
+      'JosefGlatz\\Iconcheck\\' => 'Classes',
+    ),
+  ),
+);
