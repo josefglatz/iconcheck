a39e486a5a94558df5ef1f329d370e95b9d5b25c
Josef Glatz <j.glatz@supseven.at>
2018-07-19 16:58:39 +0200

    [FEATURE] Simple filter function
    TODO: Make input backend like (with clear feature and styling)
    
    - Add a simple small box to search within the sown icon identifiers
      in the module. (Search by CSS selector and jQuery)
    - Remove superfluous .panel div in template
    - extend TYPO3/CMS/Iconcheck/CopyToClipboard
    
    Related: #5

--- Resources/Public/JavaScript/CopyToClipboard.js
+++ Resources/Public/JavaScript/CopyToClipboard.js
@@ -4,19 +4,25 @@ define([
 ], function ($, clipboardjs) {
     'use strict';
 
-    var container = $('#container-content'),
-        hiddenMarkupToShow = $('.show-when-copy-paste-supported');
+    if ($('.t3js-clearable').length) {
+        require(['TYPO3/CMS/Backend/jquery.clearable'], function() {
+            $('.t3js-clearable').clearable();
+        });
+    }
 
     // Show copy functionality if it's supported by the browser
-    if(clipboardjs.isSupported()) {
+    if (clipboardjs.isSupported()) {
+        var container = $('#container-content'),
+            hiddenMarkupToShow = $('.show-when-copy-paste-supported');
 
-        // initialize UI
-        adoptUI();
+        // add class for better UI styling if the feature is supported
+        container.toggleClass('copy-paste-supported');
+        // show hidden markup
+        hiddenMarkupToShow.toggleClass('hidden');
 
         var clipboard = new clipboardjs('.click-to-copy');
 
-        // Show feedback on success
-        clipboard.on('success', function(e) {
+        clipboard.on('success', function (e) {
             top.TYPO3.Notification.success(
                 'Identifier copied to clipboard',
                 '"' + e.text + '"'
@@ -25,33 +31,43 @@ define([
             e.clearSelection();
         });
 
-        // Log event to console and disable the functionality completely
-        clipboard.on('error', function(e) {
-
+        clipboard.on('error', function (e) {
+            // Log event to console and disable the functionality completely
             console.error('Action:', e.action);
             console.error('Trigger:', e.trigger);
-
             top.TYPO3.Notification.error(
                 'Copying identifier to clipboard failed',
                 'The functionality was removed therefore. ' +
                 'Please use your OS specific workflow to select ' +
                 'and copy text in this module manually.'
             );
-
-            // Disable clipboard.js functionality
             clipboard.destroy();
-            // uninitialize UI
-            adoptUI();
+            container.toggleClass('copy-paste-supported');
+            hiddenMarkupToShow.toggleClass('hidden');
         });
     }
 
-    /**
-     * Adopt UI markup which is used on
-     * - initialization
-     * - and if the copy to clipboard functionality has errors
-     */
-    function adoptUI() {
-        container.toggleClass('copy-paste-supported');
-        hiddenMarkupToShow.toggleClass('hidden');
-    }
+    // Add simple search functionality
+    $(document).on('keyup', '#t3js-icon-search', function (e) {
+        e.preventDefault();
+        var that = $(this);
+        setTimeout(function () {
+            if (that.val() !== '') {
+            	// Show only applicable items
+                $('[data-icon-identifier]').show().not('[data-icon-identifier*="' + that.val() + '"]').hide();
+            } else {
+            	// Show all items if input field was cleared
+                $('[data-icon-identifier]').show();
+            }
+            // Hide headers if none of it's icons is actually shown
+            $('.icon-list').each(function () {
+                var count = $('.icon-container:visible', this).length;
+                if (count <= 0) {
+                    $(this).prev().addClass('hidden');
+                } else {
+                    $(this).prev().removeClass('hidden');
+                }
+            });
+        }, 200);
+    });
 });
