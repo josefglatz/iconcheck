a07a9a452b7f59ba747e68412289dec7d2aa934a
Josef Glatz <j.glatz@supseven.at>
2023-05-16 11:24:11 +0200

    [TASK] Raise version number

--- ext_emconf.php
+++ ext_emconf.php
@@ -7,7 +7,7 @@ $EM_CONF[$_EXTKEY] = [
     'author_email' => 'jousch@gmail.com',
     'author_company' => 'josefglatz.at',
     'state' => 'stable',
-    'version' => '2.2.0',
+    'version' => '2.2.1',
     'constraints' =>
         [
             'depends' =>
